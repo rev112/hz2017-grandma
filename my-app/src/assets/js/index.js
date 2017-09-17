77dbb994dee6133cf59ff495bd2d91c5afa9b425
Anton Ovchinnikov <revolver112@gmail.com>
2017-09-17 08:02:09 +0200

    Add security status and device type to the camera view

--- my-app/src/assets/js/index.js
+++ my-app/src/assets/js/index.js
@@ -1,9 +1,9 @@
 var marker_data = [
-  {id: '02', color: 'red'},
-  {id: '10', color: 'blue'},
-  {id: '13', color: 'green'},
-  {id: '34', color: 'red'},
-  {id: '53', color: 'blue'}
+  {id: '02', name: 'AVM FRITZ!Box 5490', type: 'Wireless router', secure: true},
+  {id: '10', name: 'Philips Hue Bridge', type: 'Smart home hub', secure: false},
+  {id: '13', name: 'Nokia Body', type: 'Wi-Fi Scale', secure: true},
+  {id: '34', name: 'Smart-me Smart Plug', type: 'Energy meter', secure: false},
+  {id: '53', name: 'Piper Classic', type: 'Security system', secure: true}
 ];
 
 var state_found = false;
@@ -17,11 +17,23 @@ function add_marker_nodes() {
       .attr('url', 'patterns/' + marker_obj.id + '.patt')
       .attr('id', 'marker-' + marker_obj.id)
       .attr('num_id', marker_obj.id);
-    var $text_el = $('<a-text side="double" position="0.5 1 0" rotation="-90 0 0" />')
-      .attr('color', marker_obj.color)
-      .attr('value', 'Marker id: ' + marker_obj.id);
 
+    var $text_el = $('<a-text side="double" position="0.5 1 0" rotation="-90 0 0" />')
+      .attr('color', 'blue')
+      .attr('value', 'Name: ' + marker_obj.name);
     $el.append($text_el);
+    var $el_type = $('<a-text side="double" position="0.5 1 0.3" rotation="-90 0 0" />')
+      .attr('color', 'blue')
+      .attr('value', 'Type: ' + marker_obj.type);
+    $el.append($el_type);
+
+    var secure_color = marker_obj.secure ? 'green' : 'red';
+    var secure_status = marker_obj.secure ? 'secure' : 'insecure';
+    var $text_secure = $('<a-text side="double" position="0.5 1 0.6" rotation="-90 0 0" />')
+      .attr('color', secure_color)
+      .attr('value', 'Status: ' + secure_status);
+    $el.append($text_secure);
+
     $scene.append($el);
   }
 }
@@ -38,18 +50,6 @@ $(document).ready(function() {
       if (!state_found) {
         state_found = true;
         console.log('Status changed: visible');
-
-        // Run AJAX query to get the device information
-        var url = 'https://localhost:5000/devices/a/' + mid_num;
-        $.ajax({
-          type: "GET",
-          contentType: "application/json",
-          url: url,
-          success: function(response){
-            console.log('AJAX query: success');
-          },
-          error: function(response){}
-        });
       }
     }
     else {
